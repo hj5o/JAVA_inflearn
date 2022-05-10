6408eaaf6cfc6ae0f7efd7add848b9e6300c287d
hj5o <dataliteracy@naver.com>
2022-05-10 17:34:31 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용

--- 스프링 핵심 원리/core/src/main/java/hello/core/OrderApp.java
+++ 스프링 핵심 원리/core/src/main/java/hello/core/OrderApp.java
@@ -4,23 +4,29 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class MemberApp {
+public class OrderApp {
     public static void main(String[] args) {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        OrderService orderService = appConfig.orderService();
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findMember(1L);
-        System.out.println("new member = " + member.getName());
-        System.out.println("find member = " + findMember.getName());
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
+        System.out.println("order = " + order);
+        System.out.println("ordercalculatePrice = " + order.calculatePrice());
     }
 }
